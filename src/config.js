7f0014442d18e2229f0efcc015e30c1e24fbe7fb
Lukas Trumm <lukas.trumm@domat.cz>
2019-02-11 22:11:38 +0100

    Add environments support

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-module.exports.config = {
+export const config = {
   uvodniText:
     'Přihlašuji svého syna na letní stanový tábor 24. skautského oddílu ve Vranicích u&nbsp;Jarošova, který se koná od soboty 29. června do neděle 14. července 2019.',
   titulek: 'Přihláška na tábor',
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,13 @@
 import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 let _firebaseApp = null
 let _db = null
 
 export async function initializeFirebase() {
-  const response = await fetch('/__/firebase/init.json')
-  _firebaseApp = firebase.initializeApp(response.json())
+  const response = await fetch(process.env.VUE_APP_FIREBASE_CONFIG)
+  const json = await response.json()
+  _firebaseApp = firebase.initializeApp(json)
   _db = _firebaseApp.firestore()
 }
 
