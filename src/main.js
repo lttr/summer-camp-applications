ba342b297c3b5d7d624c4e4ee713b9acca5531df
Lukas Trumm <lukas.trumm@domat.cz>
2019-05-06 10:24:24 +0200

    Remove vuefire

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { firestorePlugin } from 'vuefire'
 import { initializeFirebase, initializeDatabase } from './firebase'
 
 Vue.config.productionTip = false
-
-Vue.use(firestorePlugin)
 
 async function initializeApp() {
   await initializeFirebase()
