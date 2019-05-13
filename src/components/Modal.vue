d306cdafcc2c899de0be45a9e131a29256559b6c
Lukas Trumm <lukas.trumm@domat.cz>
2019-05-13 06:45:31 +0200

    Use bulma's modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,25 +1,25 @@
 <template>
-  <transition name="modal">
-    <div class="modal-mask">
-      <div class="modal-wrapper">
-        <div class="modal-container">
-          <div class="modal-header">
-            <slot name="header"></slot>
-          </div>
+  <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">
+          <slot name="header"></slot>
+        </p>
+        <button class="delete" @click="$emit('close')"></button>
+      </header>
 
-          <div class="modal-body">
-            <slot name="body"></slot>
-          </div>
-
-          <div class="modal-footer">
-            <slot name="footer">
-              <button class="button modal-default-button" @click="$emit('close')">OK</button>
-            </slot>
-          </div>
-        </div>
+      <div class="modal-card-body">
+        <slot name="body"></slot>
       </div>
+
+      <footer class="modal-card-foot">
+        <slot name="footer">
+          <button class="button modal-default-button" @click="$emit('close')">OK</button>
+        </slot>
+      </footer>
     </div>
-  </transition>
+  </div>
 </template>
 
 <script>
@@ -27,65 +27,3 @@ export default {
   name: 'Modal',
 }
 </script>
-
-<style scoped>
-.modal-mask {
-  position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: table;
-  transition: opacity 0.3s ease;
-}
-
-.modal-wrapper {
-  display: table-cell;
-  vertical-align: middle;
-}
-
-.modal-container {
-  display: flex;
-  flex-direction: column;
-  max-width: 90%;
-  margin: 0px auto;
-  padding: 20px 30px;
-  background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-  transition: all 0.3s ease;
-}
-
-.modal-body {
-  margin: 20px 0;
-}
-
-.modal-default-button {
-  float: right;
-}
-
-/*
- * The following styles are auto-applied to elements with
- * transition="modal" when their visibility is toggled
- * by Vue.js.
- *
- * You can easily play with the modal transition by editing
- * these styles.
- */
-
-.modal-enter {
-  opacity: 0;
-}
-
-.modal-leave-active {
-  opacity: 0;
-}
-
-.modal-enter .modal-container,
-.modal-leave-active .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
-}
-</style>
