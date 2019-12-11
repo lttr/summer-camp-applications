fa8cc95d1e64336eb234ef4eac406cc82ae50c28
Lukas Trumm <lukas.trumm@centrum.cz>
2019-12-11 16:23:37 +0100

    Update config for 2020

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export const config = {
   uvodniText:
-    'Přihlašuji svého syna na letní stanový tábor 24. skautského oddílu ve Vranicích u&nbsp;Jarošova, který se koná od soboty 29. června do neděle 14. července 2019.',
+    'Přihlašuji svého syna na letní stanový tábor 24. skautského oddílu ve Vranicích u&nbsp;Jarošova, který se koná od soboty 4. do neděle 19. července 2020.',
   titulek: 'Přihláška na tábor',
-  termin: '29. 6. - 14. 7. 2019',
+  termin: '4. - 19. 7. 2020',
   podtitulek: '24. skautského oddílu',
-  cena: 2800,
+  cena: 2900,
   slevaSourozenci: 100,
   slevaClenOddilu: 100,
 }
